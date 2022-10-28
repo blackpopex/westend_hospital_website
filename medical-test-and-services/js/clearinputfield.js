6a69b14679654d41d05d56c709fdbe5a9dfee6c6
Oki John <blackpopex@yahoo.com>
2022-10-28 16:22:28 +0100

    corrected font-awesome errors on all pages

--- medical-test-and-services/js/clearinputfield.js
+++
@@ -1,9 +0,0 @@
-//Clear Input field when close BTN is clicked
-$(document).ready(function () {
-  $("#searchBTN").click(function () {
-    $("#searchQuery").val("");
-    $("#showSearchResult").empty();
-    $("#searchResult").show();
-    $("#searchWrapper").removeClass("show-searchResultWrapper");
-  });
-});
